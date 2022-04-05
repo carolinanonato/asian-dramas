5a6d9663ae2be7474ce3b1ed664dc7ace2e0fc1c
Carolina Nonato <csnonato@gmail.com>
2022-04-05 15:29:15 -0400

    Link to home @ title

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link"
+
 const Header = () => {
    return ( <div className="text-center border-b border-slate-300 p-4">
-       <div className="text-3xl font-bold"> Asian Dramas </div>
+       <Link href="/"><div className="text-3xl font-bold cursor-pointer"> Asian Dramas </div></Link>
     <div className="text-gray-500 text-xl" >Tv Series</div>
     </div>
    )
