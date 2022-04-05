a30a61218baf7b742b9513fc996c75d780040d2d
Carolina Nonato <csnonato@gmail.com>
2022-04-04 20:07:20 -0400

    home completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
    return ( <div className="text-center border-b border-slate-300 p-4">
-       <div className="text-2xl font-bold"> Asian Dramas </div>
-    <div className="text-gray-500">Tv Series</div>
+       <div className="text-3xl font-bold"> Asian Dramas </div>
+    <div className="text-gray-500 text-xl" >Tv Series</div>
     </div>
    )
 }
